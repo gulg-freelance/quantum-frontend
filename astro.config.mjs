3fbccf2de6cf356ca1dd4ba6f7bc0bf70b5277cc
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-09 23:45:34 +0500

    Load contact information from CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import icon from 'astro-icon';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vr-quantum.netlify.app/",
-  integrations: [icon()],
   adapter: netlify()
 });
