4e6f56dabbe5d3e87c5156db9601a54891b386fd
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-09 23:15:28 +0500

    Update metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://vr-quantum.netlify.app/",
   integrations: [icon()],
   adapter: netlify()
 });
