0e028685d34fa8900cd7d7e133f9abb17d5f883a
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-12 03:45:43 +0500

    Make header responsive, add hamburger menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vr-quantum.netlify.app/",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [icon()]
 });
